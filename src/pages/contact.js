89671b767206b87ed26bcb185909d51b27c3aec0
brettmadrid <brettmadrid9@gmail.com>
2021-07-06 14:35:48 -0700

    contact page fleshed out

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,46 @@ import Layout from "../components/Layout"
 const Contact = () => {
   return (
     <Layout>
-      <h1>contact page</h1>
+      <main className="page">
+        <section className="contact-page">
+          <article className="contact-info">
+            <h3>Want To Get In Touch?</h3>
+            <p>
+              Prism cold-pressed pour-over, locavore polaroid flexitarian fixie
+              venmo mixtape next level hell of. Fam hammock try-hard put a bird
+              on it salvia cold-pressed.
+            </p>
+            <p>
+              Photo booth shoreditch hot chicken tacos, mustache franzen keytar
+              whatever man bun.
+            </p>
+            <p>
+              Pickled pok pok pug offal chicharrones. Lomo pitchfork pour-over,
+              williamsburg fam enamel pin kitsch celiac bitters small batch
+              shaman actually.
+            </p>
+          </article>
+          <article>
+            <form className="form contact-form">
+              <div class="form-row">
+                <label htmlFor="name">your name</label>
+                <input type="text" name="name" id="name" />
+              </div>
+              <div class="form-row">
+                <label htmlFor="email">email</label>
+                <input type="text" name="email" id="email" />
+              </div>
+              <div class="form-row">
+                <label for="message">message</label>
+                <textarea name="message" id="message"></textarea>
+              </div>
+              <button type="submit" className="btn block">
+                submit
+              </button>
+            </form>
+          </article>
+        </section>
+      </main>
     </Layout>
   )
 }
